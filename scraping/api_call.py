729961052a8bb2b24f6d3244c7ff519f97850bd5
Antonis Melis <antonismelis@outlook.com>
2023-09-08 17:03:37 +0100

    Threw in some descriptions of what the code was doing

--- scraping/api_call.py
+++ scraping/api_call.py
@@ -7,10 +7,7 @@ import json
 import matplotlib.pyplot as plt
 sys.path.append("/Users/antonis/code/Ant-mel/legendary_game_recs/")
 from preprocessing.preprocess_1_cleaning import *
-
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-GRANT_TYPE = os.getenv("GRANT_TYPE")
+from urllib.parse import quote
 
 # Function that creates a list of features
 def get_list_of_features(the_json):
@@ -40,35 +37,49 @@ def get_list_of_features(the_json):
 data = pd.read_csv('raw_data/all_game_data_v1', low_memory=False)
 df = cleaning_in_notebook(data)
 
+#Data with no reviews
+
+#Creating a mask
 df_mask = df['avg_review'] == 0
-df_good_mask = df['avg_review'] != 0
-
 df_no_reviews = df[df_mask]
-df_with_reviews = df[df_good_mask]
 
+#Sorting and removing upcoming games to reduce load
 df_sorted = df_no_reviews.sort_values('release_date')
 time_mask = df_sorted['release_date'] < "2023-08-01"
 no_duplicates_recent_games = df_sorted[time_mask].drop_duplicates('title')
-review_columsn = no_duplicates_recent_games
+list_of_titles_without_review = no_duplicates_recent_games['title']
 
-list_of_titles_without_review = review_columsn['title']
-list_of_titles_without_review
+
+#Data with reviews
+df_good_mask = df['avg_review'] != 0
+df_with_reviews = df[df_good_mask]
+with_reviews_dropped_dupes = df_with_reviews.drop_duplicates('title')
+titles_with_reviews = with_reviews_dropped_dupes['title']
+
 
 
 # Credentials for calling the API
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+GRANT_TYPE = os.getenv("GRANT_TYPE")
+
+#Generating the access token
 response = requests.post(f'https://id.twitch.tv/oauth2/token?client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&grant_type={GRANT_TYPE}')
 response_json = response.json()
-
-
 ACCESS_TOKEN = response_json['access_token']
 
 
+#This is a wrapper from IGDB just for their API
 wrapper = IGDBWrapper(CLIENT_ID, ACCESS_TOKEN)
 
 no_data = []
 list_dicts = []
 
-for game in list_of_titles_without_review:
+
+#This for loop runs game titles to get the data
+for game in titles_with_reviews[10000:20000]:
+    game_q = quote(game)
+
     try:
         the_feat = json.loads(wrapper.api_request('games',
                         f'fields aggregated_rating,aggregated_rating_count, game_engines.name, game_modes.name, multiplayer_modes, player_perspectives.name, themes.name, rating; where name = "{game}";'))
@@ -82,7 +93,7 @@ for game in list_of_titles_without_review:
         no_data.append(game)
 
 the_frame = pd.DataFrame(list_dicts)
-the_frame.to_csv('raw_data/api_on_data_without_reviews', index=False)
+the_frame.to_csv('raw_data/api_data_with_reviews_10to20k', index=False)
 
 missed = pd.DataFrame(no_data)
-missed.to_csv('raw_data/api_missed_data_without_reviews', index=False)
+missed.to_csv('raw_data/api_missed_data_with_reviews_10to20k', index=False)
