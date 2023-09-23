04f2e336a0f7491c7ef7da9a4a09082a885813e4
Antonis Melis <antonismelis@outlook.com>
2023-09-23 14:35:38 +0100

    adjusted to bring more neighbours

--- model/model_create.py
+++ model/model_create.py
@@ -21,7 +21,7 @@ def create_baseline_model(raw_data):
 def predict_baseline_model(indicies, model, reference_data, X_train):
     game = X_train[indicies:indicies+1]
 
-    ind_list = list(model.kneighbors(game,n_neighbors=11)[1][0])
+    ind_list = list(model.kneighbors(game,n_neighbors=1000)[1][0])
     prediction = reference_data.iloc[ind_list]
 
     return prediction
