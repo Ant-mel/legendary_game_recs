c8423993713ff4033999df9edb5a3c5344603c29
Antonis Melis <antonismelis@outlook.com>
2024-01-11 16:11:42 +0000

    working on automating setup

--- scraping/Link_scrape.py
+++ scraping/Link_scrape.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import pickle
+from google.cloud import bigquery
+
+import os
+
+client = bigquery.Client.from_service_account_json(os.getenv('JSON_KEY'))
 
 # On the last check, you only need to scrape the first 3337 pages before weird shit arrises
-pages = range(1,3337,1)
+pages = range(40,50,1)
 
 #Creating an empty list to add the dicts to
 links_id = []
@@ -22,11 +26,16 @@ for page in pages:
         #Scraping the Backloggd page link and IGDB game_id
         link = game.find('a', href=True)['href']
         id = game['game_id']
-        print(id)
 
         links_id.append({'link':link,
                         'game_id':id})
 
-#Saves to dataframe then stores as csv
+    print(page)
+
+#Saves to dataframe then stores on GCP
 the_frame = pd.DataFrame(links_id)
-the_frame.to_csv('raw_data/all_links_with_game_id_v1', index=False)
+dataset_ref = client.dataset('game_links_test')  # Replace with your dataset name
+table_ref = dataset_ref.table('test_links_v1')
+
+job_config = bigquery.LoadJobConfig(write_disposition='WRITE_TRUNCATE')  # Overwrite existing table
+job = client.load_table_from_dataframe(the_frame, table_ref, job_config=job_config)
